a5060687f00562e1316589849522c5bcd18b7599
8dani <da.christ@tuhh.de>
2025-02-20 09:59:22 +0100

    added automatic url updating

--- api.py
+++ api.py
@@ -1,5 +1,24 @@
 import requests
 import json
+from datetime import datetime, timedelta
+
+# Aktuelles Datum und Uhrzeit abrufen
+jetzt = datetime.now()
+
+# Erster Zeitpunkt: Morgen um 00:00 Uhr
+morgen = (jetzt + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0).isoformat()
+
+# Zweiter Zeitpunkt: Vor 7 Tagen um 00:00 Uhr
+vor_sechs_tagen = (jetzt - timedelta(days=7)).replace(hour=0, minute=0, second=0, microsecond=0).isoformat()
+
+# API-Link mit Platzhaltern
+api_url_template = "https://api.opensensorweb.de/v1/organizations/pikobytes/networks/bafg/devices/5952025/sensors/w/measurements/raw?start={start}%2B01:00&end={end}%2B01:00&interpolator=LINEAR"
+
+# Link mit den berechneten Datumswerten ersetzen
+api_url = api_url_template.format(start=vor_sechs_tagen, end=morgen)
+
+# Ergebnisse ausgeben
+print("API-Link:", api_url)
 
 # API URL (mit deinem spezifischen Parameter)
 url = "https://api.opensensorweb.de/v1/organizations/pikobytes/networks/bafg/devices/5952025/sensors/w/measurements/raw?start=2025-02-12T00:00:00%2B01:00&end=2025-02-19T22:59:59%2B01:00&interpolator=LINEAR"
