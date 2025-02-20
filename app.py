3f177206efab9340cc83107c4c91f4059e8179d5
8dani <da.christ@tuhh.de>
2025-02-20 10:11:53 +0100

    fixed url to extract csv data when updated

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import requests
 
 # Raw-Link zur CSV-Datei
-csv_url = "https://raw.githubusercontent.com/8daniel2906/csv-automation/9fb2c6fe5f2ee5c1e10ae5344d48d88b9c3dbafc/sensor_data.csv"
+csv_url = "https://raw.githubusercontent.com/8daniel2906/csv-automation/main/sensor_data.csv
+"
 
 # Lade die CSV-Datei und überprüfe den Status
 response = requests.get(csv_url)
