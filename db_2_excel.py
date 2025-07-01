4d30b973675a289352743c3d4df2eb70ef53c165
8dani <da.christ@tuhh.de>
2025-07-01 22:15:27 +0200

    comments

--- db_2_excel.py
+++ db_2_excel.py
@@ -508,4 +508,4 @@ import uvicorn
 if __name__ == "__main__":
     #uvicorn.run("db_2_excel:app", host="127.0.0.1", port=8000, reload=True)
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("db_2_excel:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("db_2_excel:app", host="0.0.0.0", port=port, reload=False)
