7e5048d0af2fb706623c2bbd8803d6842db02495
8dani <da.christ@tuhh.de>
2025-07-02 17:26:43 +0200

    comments

--- db_2_excel.py
+++ db_2_excel.py
@@ -564,6 +564,6 @@ def live2():
 
 
 if __name__ == "__main__":
-    uvicorn.run("db_2_excel:app", host="127.0.0.1", port=8000, reload=True)
-    #port = int(os.environ.get("PORT", 8000))
-    #uvicorn.run("db_2_excel:app", host="0.0.0.0", port=port, reload=False)
+    #uvicorn.run("db_2_excel:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("db_2_excel:app", host="0.0.0.0", port=port, reload=False)
